df852ad8c38d243e822795eea02d8f32fe50457c
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-04 15:29:10 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,9 +8,9 @@ load_config_file = false
 
 host = var.host
 
-#client_certificate     = var.client_certificate
-#client_key             = var.client_key
-#cluster_ca_certificate = var.cluster_ca_certificate  
+client_certificate     = local.client_certificate
+client_key             = local.client_key
+cluster_ca_certificate = local.cluster_ca_certificate  
   
 username = "ubuntu"
 password = "Jennifer123"
