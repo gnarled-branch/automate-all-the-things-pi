520b3541f1f1a85c2a2bd1c5945e2e940db2008b
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-04 09:13:24 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,4 +2,15 @@
 # # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
 # # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
 
-provider "kubernetes" {}
+provider "kubernetes" {
+  
+load_config_file = false
+
+host = var.host
+
+client_certificate     = var.client_certificate
+client_key             = var.client_key
+cluster_ca_certificate = var.cluster_ca_certificate  
+  
+  
+}
