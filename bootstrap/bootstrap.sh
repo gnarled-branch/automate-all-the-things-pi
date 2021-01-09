69020e6d219bc0fe08a870192b77ce32f31b8064
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-09 09:18:45 -0500

    Update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,4 +4,8 @@ if [[ -z $(aws s3api head-bucket --bucket automate-all-the-things-pi-terraform-s
                 echo "bucket exists"
         else
                 echo "bucket does not exist or permission is not there to view it."
+                terraform init
+                terraform apply
+                cp /remote_setup/terraformConfig.tf .
+                terraform init
 fi
