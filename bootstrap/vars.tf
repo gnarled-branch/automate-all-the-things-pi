31b5765af150a8690817ca38d732b7b3788ca7da
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-10 16:10:12 -0500

    Update vars.tf

--- bootstrap/vars.tf
+++ bootstrap/vars.tf
@@ -15,7 +15,7 @@ variable "region" {
 variable "app" {
   type        = string
   description = "Name of application"
-  default     = "automate-all-the-things-pi"
+  default     = "automate-all-the-things-pi2"
 }
 variable "zone" {
   default = "us-east-2b"
@@ -23,7 +23,7 @@ variable "zone" {
 variable "docker-image" {
   type        = string
   description = "name of the docker image to deploy"
-  default     = "chrisgallivan/automate-all-the-things-docker-pi:latest"
+  default     = "chrisgallivan/automate-all-the-things-docker-pi2:latest"
 }
 
 variable "host" {
