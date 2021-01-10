56c7a047ff7bbc70eca7241297e8d4d79514fda9
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-10 16:11:28 -0500

    Update terraformConfig.tf

--- bootstrap/remote_setup/terraformConfig.tf
+++ bootstrap/remote_setup/terraformConfig.tf
@@ -1,9 +1,9 @@
 terraform {
  backend "s3" {
-   bucket = "automate-all-the-things-pi-terraform-state"
+   bucket = "automate-all-the-things-pi2-terraform-state"
    key = "global/s3/terraform.tfstate"
    region = "us-east-2"
-   dynamodb_table = "automate-all-the-things-pi-terraform-locks"
+   dynamodb_table = "automate-all-the-things-pi2-terraform-locks"
    encrypt = true
    }
  }
