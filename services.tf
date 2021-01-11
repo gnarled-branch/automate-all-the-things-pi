33dd1059c7a2a5cfbe3635ab11b7ac06c7451ec4
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-11 18:22:54 -0500

    Update services.tf

--- services.tf
+++ services.tf
@@ -6,10 +6,11 @@ resource "kubernetes_service" "app" {
     selector = {
       app = kubernetes_deployment.app.metadata.0.labels.app
     }
-    port {
+   port {
+      node_port   = 30201
       port        = 80
       target_port = 3000
     }
-    type = "LoadBalancer"
+    type = "NodePort"
   }
 }
