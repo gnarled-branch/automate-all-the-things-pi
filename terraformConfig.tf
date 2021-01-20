4af433ca37ebb67107e46e6aca7fa4c6c57eeced
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-20 09:56:24 -0500

    Delete terraformConfig.tf

--- terraformConfig.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket = "automate-all-the-things-pi2-terraform-state"
-   key = "dev/s3/terraform.tfstate"
-   region = "us-east-2"
-   dynamodb_table = "automate-all-the-things-pi2-terraform-locks"
-   encrypt = true
-   }
- }
