813730238e4721792ecfeb58d1c341be449c6c33
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-20 09:43:34 -0500

    added remote state storage in terraform cloud

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
+terraform {
+	backend "remote" {
+		organization = "KATA-FRIDAYS" 
+		workspaces {
+			name = "automate-all-the-things-pi" 
+		}
+	}
+}
+
 provider "aws" { 
   region= var.region
   access_key= var.deployment_username
   secret_key = var.deployment_password
  }
+
+
