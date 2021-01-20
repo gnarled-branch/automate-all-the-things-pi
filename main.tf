ff17bfcae0d8401c9f187530847803c0382f6983
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-20 09:45:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+	backend "remote" {
+		organization = "KATA-FRIDAYS"
+		workspaces {
+			name = "automate-all-the-things-pi"
+		}
+	}
+}
+
+
 provider "aws" { 
   region= var.region
   access_key= var.deployment_username
