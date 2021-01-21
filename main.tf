1b0d89b361ed434af43571f60d6281ff527041ec
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-21 05:51:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ terraform {
 		}
 	}
 }
+
+provider "azurerm" {
+   client_id = var.deployment_client_id
+   client_secret = var.deployment_client_secret
+   subscription_id= var.deployment_subscription_id
+   tenant_id= var.deployment_tenant_id
+   skip_provider_registration = "true"
+   features {}
+}
+
