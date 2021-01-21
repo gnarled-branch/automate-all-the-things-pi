b46a1f1602b5939792f0283962c32977ec31fc92
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-21 06:23:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,3 +16,37 @@ provider "azurerm" {
    features {}
 }
 
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "${var.prefix}-asp"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  kind                = "xenon"
+  is_xenon            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "${var.prefix}-appservice"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.example.id}"
+
+  site_config {
+    windows_fx_version = "DOCKER|index.docker.io/chrisgallivan/automate-all-the-things-pi3:latest"
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io",
+    "DOCKER_REGISTRY_SERVER_USERNAME" = "",
+    "DOCKER_REGISTRY_SERVER_PASSWORD" = "",
+  }
+}
