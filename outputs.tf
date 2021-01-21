43f9232d4ce68ad978cbeae3a15e807a9ae08f9c
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-21 08:29:23 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "website_url" {
-  value = "${azurerm_app_service.example.default_site_hostname}"
+output "app_service_name" {
+  value = "${azurerm_app_service.main.name}"
+}
+
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.main.default_site_hostname}"
 }
