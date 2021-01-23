fc5c83d32dc0b3514f80407eae473a7e64f5f582
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-23 15:26:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.app}-resources"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_app_service_plan" "example" {
   name                = "${var.app}-asp"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
   reserved            = true
 
@@ -36,9 +36,9 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_app_service" "example" {
   name                = "${var.app}-appservice"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.example.id}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
 
   site_config {
     app_command_line = ""
