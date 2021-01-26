cf0935b93aaf0d297b7e9aa6ac1afcbe863499eb
chrisgallivan <41701655+chrisgallivan@users.noreply.github.com>
2021-01-26 12:19:53 -0500

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SampleApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Sample Angular app deployed to Azure using docker and terraform!');
   });
 });
